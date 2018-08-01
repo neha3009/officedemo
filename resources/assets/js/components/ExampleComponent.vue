daa44818ef271247090440127432124a8e9c349c
neha1539 <neha@capitalnumbers.com>
2018-08-01 18:10:24 +0530

    project

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,13 +1,55 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
+        <h1> Box Builder</h1>
+        <div class="tab-navigation">
+            <ul>
+                <li data-id="0">
+                    <span class="round"></span>
+                    <p>STEP 1</p>
+                    <p>Choose your box color</p>
+                </li>
+                <li data-id="1">
+                    <span class="round"></span>
+                    <p>STEP 2</p>
+                    <p>Choose your Items</p>
+                </li>
+                <li data-id="2">
+                    <span class="round"></span>
+                    <p>STEP 3</p>
+                    <p>Select Your Card</p>
+                </li>
+                <li data-id="3">
+                    <span class="round"></span>
+                    <p>STEP 4</p>
+                    <p>Place your Order</p>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-content">
+            <div id="panel0" class="panels">
+                <div class="box-step-details">
+                    <h2>Step 1 of 3</h2>
+                    <p>Choose your box color</p>
+                </div>
+                <div class="box-full-panel">
+                    <a href="javascript:void(0)" id="white-box">
+                      <div class="box-panel white">
+                        <div class="inner-box">
+                            <div class="inner-box___again">
+                                <p>White Box</p>
+                            </div>
+                        </div>
+                      </div>
+                    </a>
+                    <a href="javascript:void(0)" id="black-box">
+                        <div class="box-panel black">
+                            <div class="inner-box">
+                                <div class="inner-box___again">
+                                    <p>Black Box</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -15,9 +57,23 @@
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+    // export default {
+    //     mounted() {
+    //         console.log('Component mounted.')
+    //     }
+    // }
+    module.exports = {
+        data() {
+            return {
+                selectedOs: "blackbox"
+            }
+        },
+        methods: {
+
         }
     }
+
 </script>
+<style scoped>
+    .selected { border: 1px solid red; }
+</style>
